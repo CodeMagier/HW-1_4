3a50bcd72bd06fe03ec141a567c2ba29eaa41836
CodeMagier <Ios23.git@gmail.com>
2024-01-17 15:25:58 +0600

    Initial Commit

---
+++ HW 1_4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 1_4
+//
+//  Created by 1 on 17/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
